c3b902111484605d7c8672c7877e2c88c29c1dad
Arambillete, Wilman <warambillete@netsuite.com>
2024-03-03 22:04:13 -0300

    test

--- lua/nsplugin/init.lua
+++ lua/nsplugin/init.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.greet()
-	vim.api.nvim_echo({ { "Hello from MyPlugin!", "None" } }, false, {})
+	vim.api.nvim_echo({ { "Hello from NS Plugin!", "None" } }, false, {})
 end
 
 function M.setup()
-	vim.cmd([[ command! MyPluginGreet lua require'myplugin'.greet() ]])
+	vim.cmd([[ command! NSPluginGreet lua require'nsplugin'.greet() ]])
 end
 
 return M
